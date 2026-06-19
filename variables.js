d61232ff257a773630f4aba3b2a74d0253c2add6
Toheed <kalwartoheed060@gmail.com>
2026-06-19 15:08:45 +0500

    var concept explained

--- variables.js
+++ variables.js
@@ -6,5 +6,11 @@ let accountHolderName="Toheed"
 var accountBalance=1000000
 
 accountHolderName="Toheed Khan"
+
+/* var is function scoped and can be re-declared and updated
+but not recommended to use var because it can lead to bugs and confusion
+*/
+
+
 // console.table is used to print the data in table format
 console.table([accountNumber, accountHolderName])
