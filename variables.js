29d9b9b0ceed17404eb36e2021aa2cf3ba1aba8e
Toheed <kalwartoheed060@gmail.com>
2026-06-19 15:01:12 +0500

    varibles_practice

---
+++ variables.js
@@ -0,0 +1,10 @@
+// Print Satement in js
+console.log("hello Toheed")
+
+const accountNumber=345252829
+let accountHolderName="Toheed"
+var accountBalance=1000000
+
+accountHolderName="Toheed Khan"
+// console.table is used to print the data in table format
+console.table([accountNumber, accountHolderName])
